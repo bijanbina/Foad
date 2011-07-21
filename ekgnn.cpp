31d942473496aaba8c4c57d60bbbbb507cfe0862
Bijan Binaee <bijanbina@gmail.com>
2011-07-21 14:30:08 +0000

    Add new Neural Network function

--- ekgnn.cpp
+++ ekgnn.cpp
@@ -1,5 +1,11 @@
 #include "ekgnn.h"
 
+EKGNN::EKGNN(QObject *parent) :
+    QObject(parent)
+{
+    isStarted = false;
+}
+
 EKGNN::EKGNN(QString path,QObject *parent) :
     QObject(parent)
 {
@@ -9,6 +15,8 @@ EKGNN::EKGNN(QString path,QObject *parent) :
 
 void EKGNN::Train(QString path,int epoch)
 {
+    if(!isStarted)
+        return;
     trainPatch = path.toStdString().c_str();
     fann_train_data *data = fann_read_train_from_file(trainPatch);
     //Start Learn
@@ -22,10 +30,12 @@ void EKGNN::openNN(QString path)
     isStarted = true;
 }
 
-double EKGNN::getOutPut(weka_data inputData)
+vector<double> EKGNN::getOutPut(weka_data inputData,int outputSize)
 {
+    //for copy array to vector
+    vector<double> output(outputSize);
     if(!isStarted)
-        return 0;
+        return output;
     fann_type *calc_out;
     fann_type input[5];
     input[0] = inputData.sqsdetected;
@@ -34,5 +44,41 @@ double EKGNN::getOutPut(weka_data inputData)
     input[3] = inputData.Ramp;
     input[4] = inputData.Qamp;
     calc_out = fann_run(ann, input);
-    return *calc_out;
+    //copy array to vector
+    for(int i = 0;i<outputSize;i++)
+    output[i] = calc_out[i];
+    return output;
 }
+
+void EKGNN::save(QString path)
+{
+    if(!isStarted)
+        return;
+    const char *savePath = path.toStdString().c_str();
+    fann_save(ann, savePath);
+}
+
+void EKGNN::create(int num_layers, int num_input,int num_neurons_hidden,int num_output)
+{
+    ann = fann_create_standard(num_layers, num_input, num_neurons_hidden, num_output);
+    isStarted = true;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- ekgnn.h
+++ ekgnn.h
@@ -1,6 +1,9 @@
 /* EKG Neural Network File that use fann library can detect disase from EKG Feature
   author:Bijan Binaee , Reza Faramarzi
   date : 2011 27 June
+  __________________________________________________________________________
+  Revisions:
+        7/21: Add new function(Train + signals + save)
 */
 #ifndef EKGNN_H
 #define EKGNN_H
@@ -11,11 +14,13 @@ class EKGNN : public QObject
 {
     Q_OBJECT
 public:
+    explicit EKGNN(QObject *parent = 0);
     explicit EKGNN(QString path,QObject *parent = 0);
     void Train(QString path,int epoch);
     void openNN(QString path);
-    double getOutPut(weka_data inputData);
-
+    vector<double> getOutPut(weka_data inputData,int outputSize);
+    void save(QString path);
+    void create(int num_layers, int num_input,int num_neurons_hidden,int num_output);
 private:
     char const *trainPatch;
     bool isStarted;
